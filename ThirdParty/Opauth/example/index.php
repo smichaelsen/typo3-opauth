92a8e0800c8f4d1beb1dde595ecde517e04b3e0f
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2013-02-07 18:13:03 +0100

    Update Opauth library to current git master

--- ThirdParty/Opauth/example/index.php
+++ ThirdParty/Opauth/example/index.php
@@ -11,12 +11,12 @@
  * Define paths
  */
 define('CONF_FILE', dirname(__FILE__).'/'.'opauth.conf.php');
-define('OPAUTH_LIB_DIR', dirname(__FILE__).'/lib/Opauth/');
+define('OPAUTH_LIB_DIR', dirname(dirname(__FILE__)).'/lib/Opauth/');
 
 /**
 * Load config
 */
-if (!file_exists(CONF_FILE)){
+if (!file_exists(CONF_FILE)) {
 	trigger_error('Config file missing at '.CONF_FILE, E_USER_ERROR);
 	exit();
 }
@@ -27,14 +27,3 @@ require CONF_FILE;
  */
 require OPAUTH_LIB_DIR.'Opauth.php';
 $Opauth = new Opauth( $config );
-?>
-<html>
-<body>
-	<p>Log in with:</p>
-	<ul>
-		<li><a href="/facebook">Facebook</a></li>
-		<li><a href="/google">Google</a></li>
-		<li><a href="/twitter">Twitter</a></li>
-	</ul>
-</body>
-</html>
