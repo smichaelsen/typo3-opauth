06e006f341caf9a678a3571487a33823d2c2cb4a
Denys Butenko <ua13dark@gmail.com>
2013-07-18 14:44:50 +0300

    Add check to connectedStrategies.

--- Classes/Controller/AuthentificationController.php
+++ Classes/Controller/AuthentificationController.php
@@ -1,17 +1,17 @@
 <?php
-namespace T3SEO\Opauth\Controller;
+namespace Butenko\OAuth\Controller;
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 class AuthentificationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
 
 	/**
-	 * @var \T3SEO\Opauth\Opauth
+	 * @var \Butenko\OAuth\Opauth
 	 */
 	protected $opauth;
 
 	public function initializeAction() {
 		$configuration = include(ExtensionManagementUtility::extPath('opauth') . 'Configuration/OpauthConfiguration.php');
-		$this->opauth = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('T3SEO\\Opauth\\Opauth', $configuration, FALSE);
+		$this->opauth = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Butenko\\OAuth\\Opauth', $configuration, FALSE);
 	}
 
 	/**
@@ -36,13 +36,38 @@ class AuthentificationController extends \TYPO3\CMS\Extbase\Mvc\Controller\Actio
 	 */
 	public function callbackAction() {
 		$response = $this->opauth->getResponse();
-		// @todo: save the user token etc here
-		$this->closePopup();
+		//$this->opauth->debug(!$this->opauth->validate(sha1(print_r($response['auth'], true)), $response['timestamp'], $response['signature'], $reason));
+
+		if (array_key_exists('error', $response))
+        {
+            echo '<strong style="color: red;">Authentication error: </strong> Opauth returns error auth response.'."<br>\n";
+        }
+        else
+        {
+            if (empty($response['auth']) || empty($response['timestamp']) || empty($response['signature']) || empty($response['auth']['provider']) || empty($response['auth']['uid']))
+            {
+                echo '<strong style="color: red;">Invalid auth response: </strong>Missing key auth response components.'."<br>\n";
+            }
+            elseif (!$this->opauth->validate(sha1(print_r($response['auth'], true)), $response['timestamp'], $response['signature'], $reason))
+            {
+                echo '<strong style="color: red;">Invalid auth response: </strong>'.$reason.".<br>\n";
+            }
+            else
+            {
+                echo '<strong style="color: green;">OK: </strong>Auth response is validated.'."<br>\n";
+
+                $GLOBALS['BE_USER']->uc['connectedStrategies'][$response['auth']['provider']] = 1;
+
+                $GLOBALS['BE_USER']->overrideUC();
+                $GLOBALS['BE_USER']->writeUC();
+            }
+        }
+		//$this->closePopup();
 	}
 
 	public function closePopup() {
 		echo '<html><head><title>Authentication success</title></head><body onload="opener.console.log(\'hi, im the popup and im finished\');window.close();"></body></html>';
-		die();
+		//die();
 	}
 
 }
