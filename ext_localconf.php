301b533ddceb7cfef9897b32ae3dd1e2e2285a93
Denys Butenko <ua13dark@gmail.com>
2013-07-23 15:46:10 +0300

    Add storage pid parameter. Check auth service priority from extension config.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,17 @@
 <?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+$_EXTCONF = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($_EXTKEY, 'auth', 'Butenko\\Opauth\\Service\\Authentification',
 	array(
 		'title' => 'Opauth Authentication',
 		'description' => 'Opauth authentication service for Frontend and Backend',
 		'subtype' => 'getUserFE,authUserFE,getUserBE,authUserBE',
 		'available' => TRUE,
-		'priority' => 75,
+		'priority' => $_EXTCONF['priority'],
 		// Must be higher than for tx_sv_auth (50) or tx_sv_auth will deny request unconditionally
 		'quality' => 50,
 		'os' => '',
@@ -20,7 +26,7 @@ if (TYPO3_MODE === 'BE') {
 	// Add popup js to user setup module
 	$TYPO3_CONF_VARS['SC_OPTIONS']['ext/setup/mod/index.php']['setupScriptHook']['opauth'] = 'Butenko\\Opauth\\Controller\\UserSetupModuleController->jsAction';
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Butenko.' . $_EXTKEY,
+		$_EXTKEY,
 		'Auth',
 		array(
 			'Authentification' => 'authenticate,callback',
@@ -33,8 +39,8 @@ if (TYPO3_MODE === 'BE') {
 
 if (TYPO3_MODE === 'FE') {
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		'Butenko.' . $_EXTKEY,
-		'Auth',
+		$_EXTKEY,
+		'Authentification',
 		array(
 			'Authentification' => 'authenticate,callback',
 		),
