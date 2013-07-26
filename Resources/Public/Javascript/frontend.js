45ef802edd719dfd46472f50b20022dd60bde02d
Denys Butenko <ua13dark@gmail.com>
2013-07-26 17:15:14 +0300

    Use jQuery for generate auth URL.

--- Resources/Public/Javascript/frontend.js
+++ Resources/Public/Javascript/frontend.js
@@ -1,8 +1,20 @@
 $(function() {
-    $(document).on('click', '[data-authstrategy]', function() {
+    jQuery('[data-authstrategy]').click(function() {
         var el = $(this);
-        var url = '/index.php?eID=opauth&type=21071992&extensionName=Opauth&pluginName=pi1&controllerName=Authentification&actionName=' + el.data('action') + '&arguments[strategy]=' + el.data('authstrategy');
-        window.location.href = url;
+        var url = jQuery.param({
+            eID: "opauth",
+            extensionName: "Opauth",
+            pluginName: "pi1",
+            controllerName: "Authentification",
+            actionName: el.data('action'),
+            arguments: {
+                strategy: el.data('authstrategy')
+            }
+        });
+
+        //window.reload();
+        //var url = '/index.php?eID=opauth&type=21071992&extensionName=Opauth&pluginName=pi1&controllerName=Authentification&actionName=' +  + '&arguments[strategy]=' + el.data('authstrategy');
+        window.location.href = decodeURIComponent('/index.php?' + url);
         return false;
     });
 });
