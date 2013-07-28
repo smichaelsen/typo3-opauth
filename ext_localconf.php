6b1a1e04d352e7d56b9c59b09515082a722b8ac5
Denys Butenko <ua13dark@gmail.com>
2013-07-28 22:28:38 +0300

    Refactor ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,7 @@ if (!defined('TYPO3_MODE')) {
 
 $_EXTCONF = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($_EXTKEY, 'auth', 'Butenko\\Opauth\\Service\\Authentification',
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($_EXTKEY, 'auth', 'Butenko\\Opauth\\OpauthService',
 	array(
 		'title' => 'Opauth Authentication',
 		'description' => 'Opauth authentication service for Frontend and Backend',
@@ -13,10 +13,10 @@ $_EXTCONF = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])
 		'available' => TRUE,
 		// Must be higher than for tx_sv_auth (50) or tx_sv_auth will deny request unconditionally
 		'priority' => $_EXTCONF['priority'],
-		'quality' => 50,
+		'quality' => $_EXTCONF['priority'],
 		'os' => '',
 		'exec' => '',
-		'className' => 'Butenko\\Opauth\\Service\\Authentification'
+		'className' => 'Butenko\\Opauth\\OpauthService'
 	)
 );
 
@@ -24,35 +24,20 @@ if (TYPO3_MODE === 'BE') {
 	// AJAX Extbase Dispatcher
 	$TYPO3_CONF_VARS['BE']['AJAX'][$_EXTKEY] = 'Butenko\\Opauth\\Utility\\AjaxDispatcher->initAndDispatch';
 	// Add popup js to user setup module
-	$TYPO3_CONF_VARS['SC_OPTIONS']['ext/setup/mod/index.php']['setupScriptHook']['opauth'] = 'Butenko\\Opauth\\Controller\\UserSetupModuleController->jsAction';
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		$_EXTKEY,
-		'Auth',
-		array(
-			'Authentification' => 'authenticate,callback',
-		),
-		array(
-			'Authentification' => 'authenticate,callback',
-		)
-	);
+	$TYPO3_CONF_VARS['SC_OPTIONS']['ext/setup/mod/index.php']['setupScriptHook'][$_EXTKEY] = 'Butenko\\Opauth\\Controller\\UserSetupModuleController->jsAction';
 }
 
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+	$_EXTKEY,
+	'Auth',
+	array(
+		'Authentification' => 'authenticate,callback,final',
+	),
+	array(
+		'Authentification' => 'authenticate,callback,final',
+	)
+);
 
-$TYPO3_CONF_VARS['FE']['eID_include'][$_EXTKEY] = 'EXT:opauth/Classes/Utility/eIDDispatcher.php';
-if (TYPO3_MODE === 'FE') {
-
-	// For FE usage via eID
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-		$_EXTKEY,
-		'pi1',
-		array(
-			'Authentification' => 'authenticate,callback',
-		),
-		array(
-			'Authentification' => 'authenticate,callback',
-		)
-	);
-}
-
+$TYPO3_CONF_VARS['FE']['eID_include'][$_EXTKEY] = 'EXT:opauth/Classes/Utility/EidDispatcher.php';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_post_processing'][] = 'Butenko\\Opauth\\UserFunction\\Logoff->logoff';
 ?>
