a6528da6ddee1c3e272d3d4e9c266ac3a4b9cee3
Denys Butenko <ua13dark@gmail.com>
2013-07-29 02:50:18 +0300

    Fix indentation

--- Classes/Utility/EidDispatcher.php
+++ Classes/Utility/EidDispatcher.php
@@ -1,28 +1,28 @@
 <?php
 namespace Butenko\Opauth\Utility;
 
-        /**
-         * @var $TSFE \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
-         */
-        $TSFE = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController', $TYPO3_CONF_VARS, 0, 0, TRUE);
-        \TYPO3\CMS\Frontend\Utility\EidUtility::initLanguage();
+/**
+ * @var $TSFE \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
+ */
+$TSFE = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController', $TYPO3_CONF_VARS, 0, 0, TRUE);
+\TYPO3\CMS\Frontend\Utility\EidUtility::initLanguage();
 
-        // Get FE User Information
-        $TSFE->initFEuser();
-        // Important: no Cache for Ajax stuff
-        $TSFE->set_no_cache();
+// Get FE User Information
+$TSFE->initFEuser();
+// Important: no Cache for Ajax stuff
+$TSFE->set_no_cache();
 
-        //$TSFE->checkAlternativCoreMethods();
-        $TSFE->checkAlternativeIdMethods();
-        $TSFE->determineId();
-        $TSFE->initTemplate();
-        //$TSFE->getConfigArray();
-        \TYPO3\CMS\Core\Core\Bootstrap::getInstance()->loadConfigurationAndInitialize();
+//$TSFE->checkAlternativCoreMethods();
+$TSFE->checkAlternativeIdMethods();
+$TSFE->determineId();
+$TSFE->initTemplate();
+//$TSFE->getConfigArray();
+\TYPO3\CMS\Core\Core\Bootstrap::getInstance()->loadConfigurationAndInitialize();
 
-        /**
-         * Initialize Database
-         */
-        \TYPO3\CMS\Frontend\Utility\EidUtility::connectDB();
+/**
+ * Initialize Database
+ */
+\TYPO3\CMS\Frontend\Utility\EidUtility::connectDB();
 
 /** @var $dispatcher \Butenko\Opauth\Utility\AjaxDispatcher */
 $dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Butenko\\Opauth\\Utility\\AjaxDispatcher');
