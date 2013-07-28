4e44a9a3f1d156217f51fa051b4605af2860e218
Denys Butenko <ua13dark@gmail.com>
2013-07-28 22:27:32 +0300

    Rename Eid

---
+++ Classes/Utility/EidDispatcher.php
@@ -0,0 +1,30 @@
+<?php
+namespace Butenko\Opauth\Utility;
+
+        /**
+         * @var $TSFE \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
+         */
+        $TSFE = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController', $TYPO3_CONF_VARS, 0, 0, TRUE);
+        \TYPO3\CMS\Frontend\Utility\EidUtility::initLanguage();
+
+        // Get FE User Information
+        $TSFE->initFEuser();
+        // Important: no Cache for Ajax stuff
+        $TSFE->set_no_cache();
+
+        //$TSFE->checkAlternativCoreMethods();
+        $TSFE->checkAlternativeIdMethods();
+        $TSFE->determineId();
+        $TSFE->initTemplate();
+        //$TSFE->getConfigArray();
+        \TYPO3\CMS\Core\Core\Bootstrap::getInstance()->loadConfigurationAndInitialize();
+
+        /**
+         * Initialize Database
+         */
+        \TYPO3\CMS\Frontend\Utility\EidUtility::connectDB();
+
+/** @var $dispatcher \Butenko\Opauth\Utility\AjaxDispatcher */
+$dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Butenko\\Opauth\\Utility\\AjaxDispatcher');
+$dispatcher->initAndDispatch();
+?>
