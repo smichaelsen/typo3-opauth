98ccd66f5d8caf2ab43ab9237f6945bede8a70c3
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2013-09-05 09:32:13 +0200

    cleanups for Logoff.php

--- Classes/UserFunction/Logoff.php
+++ Classes/UserFunction/Logoff.php
@@ -9,14 +9,14 @@ class Logoff {
      */
     public function logoff($params, $reference) {
         /** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager  */
-        $this->objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+        $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
 
         /** @var $authService \Butenko\Opauth\OpauthService */
-        $authService = $this->objectManager->get('Butenko\\Opauth\\OpauthService');
+        $authService = $objectManager->get('Butenko\\Opauth\\OpauthService');
         $requestedFile = basename($_SERVER['REQUEST_URI']);
         $referer = basename($_SERVER['HTTP_REFERER']);
         if ($requestedFile === 'logout.php' || $referer === 'logout.php' || $referer === 'backend.php') {
-            if ($reference instanceof t3lib_beUserAuth === TRUE) {
+            if ($reference instanceof \TYPO3\CMS\Core\Authentication\BackendUserAuthentication === TRUE) {
                 $authService->setScope('be');
                 $authService->logoff();
             }
