91ec9eb732eaca49a33be194445c3245ad7b539c
Denys Butenko <ua13dark@gmail.com>
2013-09-16 18:44:38 +0300

    Add scopetype param in requested url.

--- Resources/Public/Javascript/frontend.js
+++ Resources/Public/Javascript/frontend.js
@@ -1,6 +1,6 @@
-$(function() {
+jQuery(function() {
     jQuery('[data-authstrategy]').click(function() {
-        var el = $(this);
+        var el = jQuery(this);
         var url = jQuery.param({
             eID: "opauth",
             extensionName: "Opauth",
@@ -8,13 +8,14 @@ $(function() {
             pluginName: "Auth",
             controllerName: "Authentification",
             actionName: el.data('action'),
+            scopetype: el.data('scope'),
             arguments: {
-                strategy: el.data('authstrategy')
+                strategy: el.data('authstrategy'),
             }
         });
 
         //window.reload();
         //var url = '/index.php?eID=opauth&type=21071992&extensionName=Opauth&pluginName=pi1&controllerName=Authentification&actionName=' +  + '&arguments[strategy]=' + el.data('authstrategy');
-        window.location.href = decodeURIComponent('../index.php?' + url);
+        window.location.href = decodeURIComponent('../index.php?'+url);
     });
 });
