1b154b12aa65565fa11165861f034cfb662433a0
Denys Butenko <ua13dark@gmail.com>
2013-09-17 21:24:37 +0300

    Fixed #13, #7

--- Classes/OpauthService.php
+++ Classes/OpauthService.php
@@ -5,12 +5,12 @@ use TYPO3\CMS\Core\Exception;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 
-class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
+class OpauthService extends \TYPO3\CMS\Sv\AbstractAuthenticationService {
 
 	/**
 	 * @var string
 	 */
-	protected $sessionKey = 'opauth';
+	protected $extKey = 'opauth';
 
 	/**
 	 * @var array
@@ -65,7 +65,7 @@ class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
 	/**
 	 * @var array
 	 */
-	public $response;
+	protected $response;
 
 	/**
 	 * CONSTRUCTOR
@@ -73,14 +73,7 @@ class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
 	public function __construct() {
 		session_start();
 		$this->config = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['opauth']);
-
-		$translation = array('{domain}' => $_SERVER['HTTP_HOST']);
-		$urlNames = array('returnUrl');
-		foreach ($urlNames as $urlName) {
-			$this->config[$urlName] = strtr($this->config[$urlName], $translation);
-		}
-
-		$this->setScope(strtolower($_SESSION[$this->sessionKey]['currentScope'] ? $_SESSION[$this->sessionKey]['currentScope'] : TYPO3_MODE));
+		$this->setScope(strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::_GP('scopetype') ? \TYPO3\CMS\Core\Utility\GeneralUtility::_GP('scopetype') : $_SESSION[$this->extKey]['currentScope'] ));
 		$this->mode = $this->scope === 'be' ? 'getUserBE' : 'getUserFE';
 	}
 
@@ -96,12 +89,13 @@ class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
 	 */
 	public function initAuth($subType, array $loginData, array $authInfo, \TYPO3\CMS\Core\Authentication\AbstractUserAuthentication &$pObj) {
 		// Store login and authetication data
-		$this->mode = $this->scope === 'be' ? 'getUserBE' : 'getUserFE';
 		$subType = $this->mode;
 		$this->loginData = $loginData;
 		$this->authInfo = $authInfo;
 		$this->pObj = &$pObj;
-		$this->setScope(strtolower($_SESSION[$this->sessionKey]['currentScope'] ? $_SESSION[$this->sessionKey]['currentScope'] : TYPO3_MODE));
+		$this->loginData['status'] = 'login';
+		$this->authInfo['loginType'] = strtoupper($this->scope);
+
 	}
 
 	/**
@@ -109,21 +103,22 @@ class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
 	 * @return void
 	 */
 	public function responseFromController($response) {
-		$this->response = $response;
+		// Save response to $_SESSION
+		$this->response = $_SESSION[$this->extKey]['response'] = $response;
 	}
 
 	/**
 	 * @return void
 	 */
 	public function logoff() {
-		unset($_SESSION[$this->sessionKey]['user'][$this->scope]);
+		unset($_SESSION[$this->extKey]['user'][$this->scope]);
 	}
 
 	/**
 	 * @return void
 	 */
 	public function resetScope() {
-		unset($_SESSION[$this->sessionKey]['currentScope']);
+		unset($_SESSION[$this->extKey]['currentScope']);
 	}
 
 	/**
@@ -132,46 +127,58 @@ class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
 	 */
 	public function setScope($scope) {
 		$this->scope = $scope;
-		$_SESSION[$this->sessionKey]['currentScope'] = $scope;
-	}
-
-	public function authUser(array &$user) {
-		//if ($this->response['auth']['credentials']['token']){
-			if (is_array($user)){
-				return 200;
-			}
-		//}
-		return 100;
+		$_SESSION[$this->extKey]['currentScope'] = $scope;
 	}
 
 	/**
-	 * @return string
+	 * @param string $scope
+	 * @return void
 	 */
-	public function getErrorUrl() {
-		return $this->config['errorUrl'];
+	public function getScope() {
+		return $this->scope;
+	}
+
+	/**
+	 * @return void
+	 */
+	public function authUser(array &$user) {
+		$result = 100;
+		if($this->scope === 'be'){
+			if (is_array($user)){
+				$result = 200;
+				$this->writelog(255,1,0,1, "[Opauth][BE] username with email: '%s' logged successfull.", Array($user['email']));
+			}
+			if ($result == 100) {
+				$this->writelog(255,3,3,1, "[Opauth][BE] username with email: '%s' not logged.", Array($user['email']));
+			}
+		}
+		return $result;
 	}
 
 	/**
 	 * @return array User Array or FALSE
 	 */
 	public function getUser() {
-		$data = $_SESSION[$this->sessionKey]['user'][$this->scope];
+		$user = FALSE;
+		if ($this->loginData['status'] == 'login') {
+			$data = $_SESSION[$this->extKey]['user'][$this->scope];
 
-		if ($_POST['logintype'] === 'logout') {
-			$this->logoff();
-		} else
-		if (isset($_POST['user']) === TRUE) {
-			$this->setScope('fe');
-		} elseif (isset($_POST['username']) === TRUE) {
-			$this->setScope('be');
-		} elseif ($data['email'] || $data['uid'] > 0) {
-			if ($data['uid'] > 0) {
-				return $data;
-			} else {
-				$username = $this->scope === 'fe' ? $data['email'] : substr($data['email'], 0, strpos($data['email'], '@'));
-				return $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', $this->scope . '_users', "username = '" . $username . "'");
+			if ($_POST['logintype'] === 'logout') {
+				$this->logoff();
+			} else
+			if (isset($_POST['user']) === TRUE) {
+				$this->setScope('fe');
+			} elseif (isset($_POST['username']) === TRUE) {
+				$this->setScope('be');
+			} elseif ($data['email'] || $data['uid'] > 0) {
+				if ($data['uid'] > 0) {
+					$user = $data;
+				} else {
+					$user = FALSE;
+				}
 			}
 		}
+		return $user;
 	}
 
 	/**
@@ -193,6 +200,30 @@ class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
 	}
 
 	/**
+	 * @param string $groupTitle: Get group by title or create.
+	 * @return uid of group
+	 */
+	public function getGroupOrCreate($groupTitle){
+		$group = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', $this->scope . '_groups', "title = '" . $groupTitle . "'");
+		if (!$group) {
+			$pid = $this->scope === 'fe' ? $this->config['storagePid'] : 0;
+			$group = array(
+				'pid' => $pid,
+				'title' => ucwords($groupTitle),
+				'tstamp' => time(),
+				'crdate' => time(),
+				'deleted' => 0,
+				'hidden' => 0,
+				'description' => 'Group was created with Opauth App.',
+			);
+			$GLOBALS['TYPO3_DB']->exec_INSERTquery($this->scope . '_groups', $group);
+			$uid = $GLOBALS['TYPO3_DB']->sql_insert_id();
+			$group = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', $this->scope . '_groups', 'uid = ' . intval($uid));
+		}
+		return $group['uid'];
+	}
+
+	/**
 	 * @return void
 	 */
 	public function getBackendUserInformation() {
@@ -200,17 +231,22 @@ class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
 		$provider = strtolower($this->response['auth']['provider']);
 		if ($provider === 'twitter'){
 			$username = $userInfo['nickname'];
-			$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', 'be_users', "username = '" . $username . "'");
+			$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', 'be_users', "username = '" . $username . "' AND disable = 0 AND deleted = 0");
 		} else {
 			$username = substr($userInfo['email'], 0, strpos($userInfo['email'], '@'));
 			$userInfo['email'] = filter_var($userInfo['email'], FILTER_SANITIZE_EMAIL);
-			$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', 'be_users', "email = '" . $userInfo['email'] . "'");
+			$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', 'be_users', "email = '" . $userInfo['email'] . "' AND disable = 0 AND deleted = 0");
 		}
 
 		if ($record['disable'] > 0 || $record['deleted'] > 0) {
 			return;
 		}
 		if (!$record) {
+			$groupArray = array_map('strtolower', \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $this->config['addBeUsersToGroups']));
+			foreach($groupArray as $title){
+				$groupIds[] = $this->getGroupOrCreate($title);
+			}
+
 			// user has no DB record (yet), create one using defaults registered in extension config
 			// password is not important, username is set to the user's default email address
 			// fist though, we need to fetch that information.
@@ -224,14 +260,18 @@ class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
 				'disable' => '0',
 				'deleted' => '0',
 				'pid' => 0,
-				'usergroup' => $this->config['addBeUsersToGroups'],
+				'usergroup' => implode(",", $groupIds),
 				'admin' => $this->config['createAdminBeUsers']
 			);
 			$GLOBALS['TYPO3_DB']->exec_INSERTquery('be_users', $record);
 			$uid = $GLOBALS['TYPO3_DB']->sql_insert_id();
 			$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', 'be_users', 'uid = ' . intval($uid));
 		}
-		$_SESSION[$this->sessionKey]['user']['be'] = $record;
+		if($this->scope === 'be'){
+			$_SESSION[$this->extKey]['user'][$this->scope] = $record;
+		} else {
+			throw new Exception('[BE] Scope is not correctly');
+		}
 	}
 
 	/**
@@ -239,21 +279,33 @@ class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
 	 */
 	public function getFrontendUserInformation() {
 		$userInfo = $this->response['auth']['info'];
-		$userInfo['email'] = filter_var($userInfo['email'], FILTER_SANITIZE_EMAIL);
-		$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', 'fe_users', "username = '" . $userInfo['email'] . "' AND disable = 0 AND deleted = 0");
+		$provider = strtolower($this->response['auth']['provider']);
+		if ($provider === 'twitter'){
+			$username = $userInfo['nickname'];
+			$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', $this->scope.'_users', "username = '" . $username . "' AND disable = 0 AND deleted = 0");
+		} else {
+ 			$username = $userInfo['email'] = filter_var($userInfo['email'], FILTER_SANITIZE_EMAIL);
+			$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', $this->scope.'_users', "username = '" . $userInfo['email'] . "' AND disable = 0 AND deleted = 0");
+		}
 		if (!$record) {
-				// user has no DB record (yet), create one using defaults registered in extension config
-				// password is not important, username is set to the user's default email address
-				// fist though, we need to fetch that information.
+			// Check for exist group in DB or create if not-exist.
+			$groupArray = array_map('strtolower', \TYPO3\CMS\Core\Utility\GeneralUtility::trimExplode(',', $this->config['addUsersToGroups']));
+			foreach($groupArray as $title){
+				$groupIds[] = $this->getGroupOrCreate($title);
+			}
+
+			// user has no DB record (yet), create one using defaults registered in extension config
+			// password is not important, username is set to the user's default email address
+			// fist though, we need to fetch that information.
 			$record = array(
-				'username' => $userInfo['email'],
+				'username' => $username,
 				'password' => $this->generatePassword(),
 				'name' => $userInfo['name'],
 				'email' => $userInfo['email'],
 				'disable' => '0',
 				'deleted' => '0',
 				'pid' => $this->config['storagePid'],
-				'usergroup' => $this->config['addUsersToGroups'],
+				'usergroup' => implode(",", $groupIds),
 				'tstamp' => time(),
 				'crdate' => time(),
 			);
@@ -262,9 +314,13 @@ class OpauthService extends \TYPO3\CMS\Sv\AuthenticationService {
 			}
 			$GLOBALS['TYPO3_DB']->exec_INSERTquery('fe_users', $record);
 			$uid = $GLOBALS['TYPO3_DB']->sql_insert_id();
-			$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', 'fe_users', 'uid = ' . intval($uid));
+			$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('*', $this->scope.'_users', 'uid = ' . intval($uid));
 		}
-		$_SESSION[$this->sessionKey]['user']['fe'] = $record;
+		if($this->scope === 'fe'){
+			$_SESSION[$this->extKey]['user'][$this->scope] = $record;
+		} else {
+			throw new Exception('['.strtoupper($this->scope).'] Scope is not correctly');
+		}
 	}
 }
 ?>
