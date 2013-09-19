f914cef55f011cc18a3f03f88b98be343480ebea
Denys Butenko <ua13dark@gmail.com>
2013-09-19 21:59:24 +0300

    Update setupmodule.js with using jQuery.

--- Resources/Public/Javascript/setupmodule.js
+++ Resources/Public/Javascript/setupmodule.js
@@ -1,11 +1,21 @@
-$(function() {
-	$(document).on('click', '[data-authstrategy]', function() {
-		var el = $(this);
-		var url = 'ajax.php?ajaxID=opauth&pluginName=authentification&controllerName=Authentification&actionName=' + el.data('action') + '&arguments[strategy]=' + el.data('authstrategy');
+jQuery(function() {
+	jQuery('[data-authstrategy]').click(function() {
+		var el = jQuery(this);
+		var url = jQuery.param({
+			ajaxID: "opauth",
+			pluginName: "Auth",
+			controllerName: "Authentification",
+			actionName: el.data('action'),
+			scopetype: el.data('scope'),
+			arguments: {
+				strategy: el.data('authstrategy'),
+			}
+		});
+
 		var authpopup = window.open(
-			url,
+			decodeURIComponent('ajax.php?'+url),
 			'Authenticate with ' + el.data('authstrategy'),
 			'width=640,height=480,resizable=yes'
-		);
+		)
 	});
 });
