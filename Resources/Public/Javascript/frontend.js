ce21fbac5901be0708ebce530327d46b9a94445d
Denys Butenko <ua13dark@gmail.com>
2013-09-19 22:00:19 +0300

    Convert indentation to tabs.

--- Resources/Public/Javascript/frontend.js
+++ Resources/Public/Javascript/frontend.js
@@ -1,21 +1,19 @@
 jQuery(function() {
-    jQuery('[data-authstrategy]').click(function() {
-        var el = jQuery(this);
-        var url = jQuery.param({
-            eID: "opauth",
-            extensionName: "Opauth",
-            logintype: "login",
-            pluginName: "Auth",
-            controllerName: "Authentification",
-            actionName: el.data('action'),
-            scopetype: el.data('scope'),
-            arguments: {
-                strategy: el.data('authstrategy'),
-            }
-        });
+	jQuery('[data-authstrategy]').click(function() {
+		var el = jQuery(this);
+		var url = jQuery.param({
+			eID: "opauth",
+			extensionName: "Opauth",
+			logintype: "login",
+			pluginName: "Auth",
+			controllerName: "Authentification",
+			actionName: el.data('action'),
+			scopetype: el.data('scope'),
+			arguments: {
+				strategy: el.data('authstrategy'),
+			}
+		});
 
-        //window.reload();
-        //var url = '/index.php?eID=opauth&type=21071992&extensionName=Opauth&pluginName=pi1&controllerName=Authentification&actionName=' +  + '&arguments[strategy]=' + el.data('authstrategy');
-        window.location.href = decodeURIComponent('../index.php?'+url);
-    });
+		window.location.href = decodeURIComponent('../index.php?'+url);
+	});
 });
